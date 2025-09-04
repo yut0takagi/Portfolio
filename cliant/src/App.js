74c75a59a6ac0dbf4021e2936c546bc2340c42d6
yut0takagi <yutotkg.1040@gmail.com>
2025-09-04 19:40:59 +0900

    404エラーページを追加し、HashRouterを使用するようにApp.jsを更新しました。

--- cliant/src/App.js
+++ cliant/src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React from "react"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { HashRouter as Router, Routes, Route } from "react-router-dom"
 import './App.css';
 import Home from "./pages/Home"
 import ProjectDetail from "./pages/ProjectDetail"
